3dfe21422a6becb1b89196712888a80057a67759
mr-asif-khan <itechprogramming030@gmail.com>
2025-03-15 03:45:00 +0500

    Contact Template

--- contact-template.php
+++ contact-template.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: About
+ * Template Name: Contact
  */
 
 get_header(); ?>
@@ -143,147 +143,52 @@ get_header(); ?>
         </div>
     </header>
     <!-- Header End -->
-    
-    <!-- Hero Section -->
-    <section class="about-hero-section">
-        <div class="container">
-            <div class="about-hero-section-text">
-                <h1>MEET GRANT CARDONE </h1>
-                <p>Grant Cardone is an American businessman, Equity Fund Manager ($4B Assets Under Management), Real Estate Investor, Business author and Founder of 10X Growth Conference, 10X Health System, Cardone Ventures & 10X Studios. Also author of seven best selling business books, creator of Cardone U online education and The 10X Growth Conferences (the largest business conference in the USA.)     </p>
-            </div>
-        </div>
-    </section>
-    <!-- Hero Section End -->
 
-    <!-- About Grant Cardone -->
-    <section class="about-grant-cardone">
-        <div class="container">
-            <div class="head">
-                <p>
-                    “I work with small companies and Fortune 500 companies to grow sales by finding overlooked opportunities and customizing the sales process to be more effective. I have worked with companies like Google, Sprint, Aflac, Toyota, GM, Ford, and thousands more. I own and operate seven companies that do almost $100 million in annual sales and I’m also a <i>New York Times</i>
-                    bestselling author, international speaker, & considered the top sales training and social media expert in the world today.”
-                </p>
-                <h2>“SUCCESS IS YOUR DUTY, OBLIGATION, RESPONSIBILITY.” </h2>
-                <p>– Grant Cardone</p>
-            </div>
-            <div class="con">
-                <p>
-                    Grant urges his followers and clients to make success their duty, responsibility, obligation, and to <b>rise above outdated, unworkable middle-class myths and limitations</b>
-                    in order to achieve true freedom for themselves and their families.
-                </p>
-                <p>
-                    His straight-shooting viewpoints on leadership, the economy, small business, retail sales, employment, and headlines have made him a valuable resource for media seeking commentary and insights on <b>real topics that matter.</b>
-                </p>
-            </div>
+    <!-- Contact Info -->
+    <section class="contact-info">
+      <div class="container">
+        <div class="contact-info-left">
+          <h1>Cardone EnterPrise</h1>
+          <div class="address1">
+              <ul>
+                  <li><strong>Miami Headquarters</strong></li>
+                  <li>18909 NE 29th Ave</li>
+                  <li>Aventura, FL 33180</li>
+                  <li><strong>Toll-Free</strong>: <a href="tel:8003685771"> (800) 368-5771</a></li>
+                  <li><strong>Office</strong>: <a href="tel:3107770255"> (310) 777-0255</a></li>
+                  <li><strong>Fax</strong>: <a href="fax:3107770256"> (310) 777-0256</a></li>
+              </ul>
+          </div>
+          <div class="address2">
+            <ul>
+                <li><strong>Scottsdale Headquarters</strong></li>
+                <li>16435 N. Scottsdale Road</li>
+                <li>Suite 400</li>
+                <li>Scottsdale, AZ 85254</li>
+                <li><strong>Phone</strong>: <a href="tel:6026130150"> (602) 613-0150</a></li>
+                <li><strong>Fax</strong>: <a href="fax:6026130146"> (602) 613-0146</a></li>
+            </ul>
+          </div>
         </div>
-    </section>
-    <!-- About Grant Cardone End -->
 
-    <!-- About Movement Section -->
-    <section class="about-movement-section">
-        <div class="container">
-            <div class="about-movement-head">
-                <h2>THE 10X MOVEMENT </h2>
-                <p>
-                    Beginning with his bestselling book <i>The 10X Rule</i>
-                    in 2011, Grant then launched the 10X Growth Conference beginning in 2017. This 3-day event has quickly become the world’s largest business and entrepreneur conference – learn more at <a href="http://10xgrowthcon.com" target="_blank" rel="noopener">10XGrowthCon.com</a>
-                </p>
-                <p>
-                    Grant created the 10X Growth Conference to be a place where people come to learn strategies from the most successful entrepreneurs in the world to 10X Your Business, 10X Your Income, and 10X Your Life. There will be a jammed packed lineup of world-class speakers at the next event—check out all the details <a href="http://10xgrowthcon.com" target="_blank" rel="noopener">HERE!</a>
-                </p>
-            </div>
-            <div class="about-movement-row">
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img.webp" alt="">
-                    </div>
-                    <p>Internationally renowned business and sales expert. Named the number-one sales trainer in the world and number one of the “25 Marketing Influencers to Watch in 2017” by Forbes.</p>
-                </div>
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img02.webp" alt="">
-                    </div>
-                    <p>
-                        Author of 8 sales and business books, including the <i>New York Times</i>
-                        bestseller <i>If You’re Not First, You’re Last,</i>
-                        and the Axiom Award Winner <i>Sell or Be Sold.</i>
-                    </p>
-                </div>
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img03.webp" alt="">
-                    </div>
-                    <p>Creator of Cardone University, the premier online sales training tool in the world with over 500,000 users and counting.</p>
-                </div>
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img04.webp" alt="">
-                    </div>
-                    <p>A dynamic, highly sought after, international speaker, coach and consultant. His speeches have been listened to by hundreds of millions all over the world.</p>
-                </div>
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img05.webp" alt="">
-                    </div>
-                    <p>An incredibly successful entrepreneur offering insights, tips, advice and guidance on sales, marketing, branding, real estate, relationships, investments, wealth and more.</p>
-                </div>
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img06.webp" alt="">
-                    </div>
-                    <p>Regular commentator and guest appearing on Fox News, Fox Business, CNBC and MSNBC. Contributing writer to Forbes, Success Magazine, Business Insider, Entrepreneur.com and Huffington Post.</p>
-                </div>
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img07.webp" alt="">
-                    </div>
-                    <p>Real estate investor, disrupter, mogul and founder of Cardone Capital. His private equity firm offers funding opportunities to a variety of investors in overlooked markets.</p>
-                </div>
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img08.webp" alt="">
-                    </div>
-                    <p>Philanthropist and believer in giving back as well as paying it forward. Creator of the Cardone Foundation which provides mentoring and financial literacy education to at-risk people.</p>
-                </div>
-                <div class="item">
-                    <div class="img">
-                        <img src="<?php echo get_template_directory_uri() ?>/img/gc-about-img09.webp" alt="">
-                    </div>
-                    <p>International social media influencer. With over 15,000,000 followers, fans and connections Grant Cardone has been named one of the Top 10 CEOs to follow.</p>
-                </div>
-            </div>
+        <div class="contact-info-right">
+          <?php echo do_shortcode('[contact-form-7 id="9275727" title="Contact form 1"]'); ?>
         </div>
+      </div>
     </section>
-    <!-- About Movement Section End -->
+    <!-- Contact Info End -->
 
-    <!-- About Sign Up Section -->
-    <section class="about-signup-section">
-        <div class="container">
-            <div class="about-signup-section-row">
-                <div class="about-signup-section-content">
-                    <h2>SIGN UP TODAY FOR THE 10X NATION AND RECEIVE:</h2>
-                    <ul class="list-tick">
-                        <li>Welcome Series with my free eBook: 10 Ways to Prosperity</li>
-                        <li>10X Strategy of the Week</li>
-                        <li>Exclusive deals on new products, live events, and premium products.</li>
-                        <li>Updates on new content, podcasts, and videos.</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="about-signup-section-form">
-                <?php echo do_shortcode('[contact-form-7 id="5ff4b01" title="About Form"]'); ?>
-            </div>
+    <!-- Contact Banner Section -->
+    <section class="contact-banner">
+      <div class="container">
+        <div class="contact-banner-content">
+          <h1 class="text-center">JOIN GRANT’S 10X STRATEGY OF THE WEEK</h1>
+          <p>BE PART OF THE 10X NATION.</p>
+          <a href="#">JOIN EMAIL LIST</a>
         </div>
+      </div>
     </section>
-    <!-- About Sign Up Section End -->
-
-    <!-- About Banner Section -->
-    <section class="about-banner-section">
-        <div class="container">
-            <h2>“The single biggest financial mistake I’ve made was not thinking big enough. I encourage you to go for more than a million. There is no shortage of money on this planet, only a shortage of people thinking big enough.” -GRANT CARDONE </h2>
-        </div>
-    </section>
-    <!-- About Banner Section End -->
+    <!-- Contact Banner Section End -->
 
     <!-- Footer Starts -->
     <footer class="footer_wrap">
@@ -504,4 +409,3 @@ get_header(); ?>
     </a>
 
 </div>
-<?php get_footer(); ?>
